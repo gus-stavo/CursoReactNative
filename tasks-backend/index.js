6c70cd3f6482e22a6892d0c64e3d9bd8b89f5772
gus-stavo <gustavomlstefano@gmail.com>
2021-04-14 00:01:44 -0300

    Resumo de Express e Instalado PSQL
    Resumo de Express e Instalado PSQL

--- tasks-backend/index.js
+++ tasks-backend/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
+/*const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-    res.status(200).send('Meu Backend!')
+app.use(meuJson())
+app.use(bodyParser.json())
+
+function meuJson() {
+    return (req, res, next) => {
+        console.log('Antes de tudo: Meu middleware...')
+        next()
+    }
+}
+
+app.post('/blabla/:valor', (req, res, next) => {
+    console.log('Func 0')
+    next()
 })
+
+app.post('/blabla/:valor', (req, res, next) => {
+    console.log('Func 1')
+    res.status(200).send('Meu Backend = ' 
+        + JSON.stringify(req.body.dependentes[0].nome))
+})
+
+app.post('/blabla/:valor', (req, res) => {
+    console.log('Func 3')
+})*/
 
 app.listen(3000, () => {
     console.log('Backend executando...')
