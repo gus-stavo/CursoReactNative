40bf0a5bfa81deb6121ca5e6f6dd36a6046c7abd
gus-stavo <gustavomlstefano@gmail.com>
2021-04-22 19:13:33 -0300

    Revert "Revert "Iniciando integração Tasks""
    This reverts commit 07e8a8d98d2c6b2921277404acfbbe6e82d35d5c.

--- tasks-backend/api/auth.js
+++ tasks-backend/api/auth.js
@@ -7,7 +7,7 @@ module.exports = app => {
         if (!req.body.email || !req.body.password) return res.status(400).send('Dados incompletos!')
 
         const user = await app.db('users')
-            .where({ email: req.body.email })
+            .whereRaw("email = LOWER(?)", req.body.email)
             .first()
 
         if (user) {
