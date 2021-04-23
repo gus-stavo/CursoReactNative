94adc0f37da72fe5fd65c76978c37a0afbb17044
gus-stavo <gustavomlstefano@gmail.com>
2021-04-22 22:09:43 -0300

    Finalização da Autenticação
    Finalização da Autenticação

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -64,6 +64,17 @@ export default class Auth extends Component {
     }
 
     render() {
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >=6)
+
+        if(this.state.stageNew) {
+            validations.push(this.state.name && this.state.name.trim().length >=3)
+            validations.push(this.state.confirmPassword === this.state.password)
+        }
+
+        const validForm = validations.reduce((t, a) => t && a)
+
         return (
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
@@ -92,8 +103,9 @@ export default class Auth extends Component {
                             style={styles.input} secureTextEntry
                             onChangeText={confirmPassword => this.setState({ confirmPassword })} />
                     }
-                <TouchableOpacity onPress={this.signinOrSignup}>
-                    <View style={styles.button}>
+                <TouchableOpacity onPress={this.signinOrSignup}
+                    disabled={!validForm}>
+                    <View style={[styles.button, validForm ? {} : { backgroundColor: '#AAA' }]}>
                         <Text style={styles.buttonText}>
                             {this.state.stageNew ? 'Registrar' : 'Entrar'}
                         </Text>
